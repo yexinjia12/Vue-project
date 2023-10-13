f5d8bbb28f7422154b37fd30d5dac62d00744be4
wxf <wangxiaomei722@gmail.com>
2023-10-13 21:26:10 +0800

    模板引用

---
+++ vue3-demo/src/components/Day2/template-reference/child-com.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+// 默认情况下，<script setup>语法糖下 组件内部的方法和属性不开放给父组件访问
+// 可以通过编译宏函数defineExpose指定哪些属性和方法允许访问
+const count = ref(100)
+const setCount = () => {
+  count.value++
+}
+defineExpose({
+  count,
+  setCount
+})
+</script>
+<template>
+  <div>
+    <h2>子组件App - h2</h2>
+  </div>
+</template>
+<style lang='less'></style>
---
+++ vue3-demo/src/components/Day2/template-reference/parent-com.vue
@@ -0,0 +1,21 @@
+<script setup>
+// 组件不再需要components注册了，可以直接使用
+import ChildCom from './child-com.vue'
+import { onMounted, ref } from 'vue'
+const h2Ref = ref(null)
+const childRef = ref(null)
+
+// 组件挂载完毕才能获取到模板引用
+onMounted(() => {
+  console.log('h2', h2Ref.value);
+  console.log('child-com', childRef.value.count);
+})
+</script>
+<template>
+  <div>
+    <h2 ref="h2Ref">父组件App - h2</h2>
+    <!-- 接收子组件数据跟vue2语法一样 -->
+    <ChildCom ref="childRef" />
+  </div>
+</template>
+<style lang='less'></style>
