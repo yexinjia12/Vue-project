65001929c297e03fb152f24f6d1b850db0309c34
wxf <wangxiaomei722@gmail.com>
2023-10-13 21:24:26 +0800

    子组件向父组件传值

---
+++ vue3-demo/src/components/Day2/child-to-parent/child-com.vue
@@ -0,0 +1,14 @@
+<script setup>
+// 使用 编译器宏函数defineEmits 向父组件传值
+const emits = defineEmits(['get-message'])
+const setMessage = () => {
+  emits('get-message', 'this is child message')
+}
+</script>
+<template>
+  <div>
+    <h2>子组件App</h2>
+    <button @click="setMessage">点击子组件向父组件传值</button>
+  </div>
+</template>
+<style lang='less'></style>
---
+++ vue3-demo/src/components/Day2/child-to-parent/parent-com.vue
@@ -0,0 +1,15 @@
+<script setup>
+// 组件不再需要components注册了，可以直接使用
+import ChildCom from './child-com.vue'
+const getMessage = (msg) => {
+  console.log(msg);
+}
+</script>
+<template>
+  <div>
+    <h2>父组件App</h2>
+    <!-- 接收子组件数据跟vue2语法一样 -->
+    <ChildCom @get-message="getMessage" />
+  </div>
+</template>
+<style lang='less'></style>
