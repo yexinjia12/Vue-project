16d3af8adc64345f991a6bcddbaa79dce9bae3a5
wxf <wangxiaomei722@gmail.com>
2023-10-13 21:24:58 +0800

    父组件向子组件传值

---
+++ vue3-demo/src/components/Day2/parent-to-child/child-com.vue
@@ -0,0 +1,15 @@
+<script setup>
+// 使用 编译器宏函数defineProps 接收父组件传递的数据
+const props = defineProps({
+  message: String,
+  count: Number
+})
+console.log('子组件script中调用父组件传入的值', props);
+</script>
+<template>
+  <div>
+    <h2>子组件App</h2>
+    <div>父组件传入的参数 - {{ message }} - {{ count }}</div>
+  </div>
+</template>
+<style lang='less'></style>
---
+++ vue3-demo/src/components/Day2/parent-to-child/parent-com.vue
@@ -0,0 +1,17 @@
+<script setup>
+// 组件不再需要components注册了，可以直接使用
+import ChildCom from './child-com.vue'
+import { ref } from 'vue'
+const count = ref(100)
+setTimeout(() => {
+  count.value = 222
+}, 3000)
+</script>
+<template>
+  <div>
+    <h2>父组件App</h2>
+    <!-- 传值方式和Vue2一样 -->
+    <ChildCom :count="count" message="this is parent message" />
+  </div>
+</template>
+<style lang='less'></style>
