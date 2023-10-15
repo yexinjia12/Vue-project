78c9bde1318fbe10316e198f3965fe379148b321
wxf <wangxiaomei722@gmail.com>
2023-10-15 23:31:28 +0800

    Layout一级导航渲染

--- vue-rabbit/src/views/Layout/components/LayoutHeader.vue
+++ vue-rabbit/src/views/Layout/components/LayoutHeader.vue
@@ -1,5 +1,17 @@
 <script setup>
+import { onMounted, ref } from 'vue'
+import { getCategoryAPI } from "@/apis/layout.js";
 
+// 获取一级导航菜单数据
+const categoryList = ref([])
+const getCategory = async () => {
+  const res = await getCategoryAPI()
+  categoryList.value = res.result;
+}
+
+onMounted(() => {
+  getCategory()
+})
 </script>
 
 <template>
@@ -9,17 +21,8 @@
         <RouterLink to="/">小兔鲜</RouterLink>
       </h1>
       <ul class="app-header-nav">
-        <li class="home">
-          <RouterLink to="/">首页</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">居家</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">美食</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">服饰</RouterLink>
+        <li class="home" v-for="item in categoryList" :key="item.id">
+          <RouterLink to="/">{{ item.name }}</RouterLink>
         </li>
       </ul>
       <div class="search">
@@ -133,4 +136,5 @@
       }
     }
   }
-}</style>
+}
+</style>
