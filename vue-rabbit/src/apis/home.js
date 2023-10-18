c6639efdfeb2a510e571b043697c494aa632b447
wxf <wangxiaomei722@gmail.com>
2023-10-18 23:14:27 +0800

    home-product列表渲染

--- vue-rabbit/src/apis/home.js
+++ vue-rabbit/src/apis/home.js
@@ -3,20 +3,27 @@ import httpIntance from '@/utils/http'
 // 获取轮播图数据接口
 export const getBannerAPI = () => {
   return httpIntance({
-    url: 'home/banner'
+    url: 'home/banner',
   })
 }
 
 // 获取新鲜好物接口
 export const getNewAPI = () => {
   return httpIntance({
-    url: '/home/new'
+    url: '/home/new',
   })
 }
 
 // 获取人气推荐接口
 export const getHotAPI = () => {
   return httpIntance({
-    url: 'home/hot'
+    url: 'home/hot',
+  })
+}
+
+// 获取产品列表接口
+export const getProductAPI = () => {
+  return httpIntance({
+    url: 'home/goods',
   })
 }
