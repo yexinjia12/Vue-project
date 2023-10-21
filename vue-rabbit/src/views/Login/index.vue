331ac3557f863b4b1cdf5aeb97a6f010a1ecb4b3
wxf <wangxiaomei722@gmail.com>
2023-10-21 17:18:16 +0800

    登录-表单基础校验

--- vue-rabbit/src/views/Login/index.vue
+++ vue-rabbit/src/views/Login/index.vue
@@ -1,4 +1,21 @@
 <script setup>
+import { ref } from 'vue'
+// 表单对象
+const form = ref({
+  account: '',
+  password: ''
+})
+
+// 表单规则对象
+const rules = {
+  account: [
+    { required: true, message: '用户名不能为空', trigger: 'blur' },
+  ],
+  password: [
+    { required: true, message: '密码不能为空', trigger: 'blur' },
+    { min: 6, max: 14, message: '密码长度6-14个字符', trigger: 'blur' },
+  ]
+}
 
 </script>
 
@@ -24,12 +41,12 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px" status-icon>
-              <el-form-item label="账户">
-                <el-input />
+            <el-form :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
+              <el-form-item prop="account" label="账户">
+                <el-input v-model="form.account" />
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input />
+              <el-form-item prop="password" label="密码">
+                <el-input v-model="form.password" />
               </el-form-item>
               <el-form-item label-width="22px">
                 <el-checkbox size="large">
