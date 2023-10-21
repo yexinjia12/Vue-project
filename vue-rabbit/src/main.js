7dced310b555f25edb8a3f0e131dc734560ccf9d
wxf <wangxiaomei722@gmail.com>
2023-10-21 19:02:56 +0800

    登录-Pinia 存储的持久化配置

--- vue-rabbit/src/main.js
+++ vue-rabbit/src/main.js
@@ -2,6 +2,7 @@ import '@/styles/common.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
@@ -11,9 +12,12 @@ import { lazyPlugin } from './directives'
 // 自定义组件
 import { componentPlugin } from './components'
 
-const app = createApp(App)
+const pinia = createPinia()
+// 注册 Pinia 存储的持久化配置
+pinia.use(piniaPluginPersistedstate)
 
-app.use(createPinia())
+const app = createApp(App)
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentPlugin)
--- vue-rabbit/src/stores/user.js
+++ vue-rabbit/src/stores/user.js
@@ -3,14 +3,22 @@ import { defineStore } from 'pinia'
 import { loginAPI } from '@/apis/user'
 import { ref } from 'vue'
 
-export const useUserStore = defineStore('user', () => {
-  const userInfo = ref({})
-  const getUserInfo = async ({ account, password }) => {
-    const res = await loginAPI({ account, password })
-    userInfo.value = res.result
+export const useUserStore = defineStore(
+  'user',
+  () => {
+    const userInfo = ref({})
+    const getUserInfo = async ({ account, password }) => {
+      const res = await loginAPI({ account, password })
+      userInfo.value = res.result
+    }
+    return {
+      userInfo,
+      getUserInfo,
+    }
+  },
+  // 持久化配置
+  // 在设置state的时候会自动把数据同步给localStorage，在获取state数据的时候会优先从localStorage中取
+  {
+    persist: true,
   }
-  return {
-    userInfo,
-    getUserInfo,
-  }
-})
+)
