3539de7e3849d5dd70ba071e04ce1f0b7d076913
wxf <wangxiaomei722@gmail.com>
2023-10-27 15:57:03 +0800

    结算-地址切换

--- vue-rabbit/src/views/Checkout/index.vue
+++ vue-rabbit/src/views/Checkout/index.vue
@@ -13,6 +13,9 @@ const getCheckoutInfo = async () => {
   }) || checkInfo.value.userAddresses[0]
 }
 
+// 切换地址弹窗显示
+const changeAddressDialog = ref(false)
+
 onMounted(() => getCheckoutInfo())
 
 
@@ -35,7 +38,7 @@ onMounted(() => getCheckoutInfo())
               </ul>
             </div>
             <div class="action">
-              <el-button size="large" @click="toggleFlag = true">切换地址</el-button>
+              <el-button size="large" @click="changeAddressDialog = true">切换地址</el-button>
               <el-button size="large" @click="addFlag = true">添加地址</el-button>
             </div>
           </div>
@@ -116,6 +119,23 @@ onMounted(() => getCheckoutInfo())
     </div>
   </div>
   <!-- 切换地址 -->
+  <el-dialog title="切换收货地址" width="30%" center>
+    <div class="addressWrapper">
+      <div class="text item" v-for="item in checkInfo.userAddresses" :key="item.id">
+        <ul>
+          <li><span>收<i />货<i />人：</span>{{ item.receiver }} </li>
+          <li><span>联系方式：</span>{{ item.contact }}</li>
+          <li><span>收货地址：</span>{{ item.fullLocation + item.address }}</li>
+        </ul>
+      </div>
+    </div>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="changeAddressDialog = false">取消</el-button>
+        <el-button type="primary">确定</el-button>
+      </span>
+    </template>
+  </el-dialog>
   <!-- 添加地址 -->
 </template>
 
